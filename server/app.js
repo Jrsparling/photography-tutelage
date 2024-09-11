95a06130be33d574eccc0eb2a2bced417924de30
denningcars <denningcars@gmail.com>
2024-09-10 19:07:47 -0500

    implemented GridFS

---
+++ server/app.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const config = require('./config');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+const imageRouter = require('./routes/image');
+
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+const mongoose = require('mongoose');
+
+const url = config.mongoURI;
+const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const startApolloServer = async () => {
+    await server.start();
+  
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(express.json());
+    app.use(methodOverride('_method'));
+  
+    // Serve up static assets
+    app.use('/images', express.static(path.join(__dirname, '../client/images')));
+  
+    app.use('/graphql', expressMiddleware(server, {
+      context: authMiddleware
+    }));
+  
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      });
+    }
+  
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      });
+    });
+  };
+  
+  // Call the async function to start the server
+  startApolloServer();
+  
+
+connect.then(() => {
+    console.log('Connected to database: GridApp');
+  }, (err) => console.log(err));
+  
+
+const storage = new GridFsStorage({
+    url: config.mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+
+const upload = multer({ storage });
+
+app.use('/upload', imageRouter(upload));
+
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
